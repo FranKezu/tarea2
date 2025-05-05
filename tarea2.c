5dab6effef2d4d1e11bda2e16cce3ff88f7eb289
Your Name <you@example.com>
2025-05-04 23:54:02 -0400

    funcion buscar por genero

--- tarea2.c
+++ tarea2.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // gcc tarea2.c tdas/list.c tdas/map.c tdas/extra.c -o tarea2
 
@@ -14,7 +15,7 @@ typedef struct {
   char track_name[100];
   float tempo;
   char track_genre[100];
-} Film;
+} Song;
 
 // Menú principal
 void mostrarMenuPrincipal() {
@@ -71,7 +72,7 @@ void cargar_musica(Map *music_byid, Map *music_bygenres) {
 
   long contador = 0;
   while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
-    Film *song = (Film *)malloc(sizeof(Film));
+    Song *song = (Song *)malloc(sizeof(Song));
     strncpy(song->id, campos[0], 99);
     song->id[99] = '\0';
     
@@ -103,12 +104,13 @@ void cargar_musica(Map *music_byid, Map *music_bygenres) {
     }
     contador++;
     if (contador % 1000 == 0) {
-      printf("Cargando canciones (%ld / 114001)\n", contador);
+      printf("Cargando canciones (%ld / 114000)\n", contador);
     }
+
+    if(contador == 10000) break;
   }
   fclose(archivo); 
   printf("Canciones cargadas exitosamente (%ld)\n", contador);
-  printf("Presione una tecla para continuar...\n");
   while (getchar() != '\n');
   getchar();
 }
@@ -117,7 +119,7 @@ void cargar_musica(Map *music_byid, Map *music_bygenres) {
  * Busca y muestra la información de una película por su ID en un mapa.
  */
 /*
-void buscar_por_id(Map *music_byid) {
+void buscar_por_artista(Map *music_byid) {
   char id[10]; // Buffer para almacenar el ID de la película
 
   // Solicita al usuario el ID de la película
@@ -130,7 +132,7 @@ void buscar_por_id(Map *music_byid) {
   // Si se encontró el par clave-valor, se extrae y muestra la información de la
   // película
   if (pair != NULL) {
-    Film *peli =
+    Song *peli =
         pair->value; // Obtiene el puntero a la estructura de la película
     // Muestra el título y el año de la película
     printf("Título: %s, Año: %d\n", peli->artists, peli->year);
@@ -139,28 +141,67 @@ void buscar_por_id(Map *music_byid) {
     printf("La película con id %s no existe\n", id);
   }
 }
-
+*/
 void buscar_por_genero(Map *music_bygenres) {
+
+  if(map_first(music_bygenres) == NULL){
+      printf("NO SE HAN CARGADO CANCIONES\n");
+      return;
+  }    
+  char *genres[114] = {
+    "acoustic", "afrobeat", "alt-rock", "alternative", "ambient",
+    "anime", "black-metal", "bluegrass", "blues", "brazil",
+    "breakbeat", "british", "cantopop", "chicago-house", "children",
+    "chill", "classical", "club", "comedy", "country",
+    "dance", "dancehall", "death-metal", "deep-house", "detroit-techno",
+    "disco", "disney", "drum-and-bass", "dub", "dubstep",
+    "edm", "electro", "electronic", "emo", "folk",
+    "forro", "french", "funk", "garage", "german",
+    "gospel", "goth", "grindcore", "groove", "grunge",
+    "guitar", "happy", "hard-rock", "hardcore", "hardstyle",
+    "heavy-metal", "hip-hop", "honky-tonk", "house", "idm",
+    "indian", "indie", "indie-pop", "industrial", "iranian",
+    "j-dance", "j-idol", "j-pop", "j-rock", "jazz",
+    "k-pop", "kids", "latin", "latino", "malay",
+    "mandopop", "metal", "metalcore", "minimal-techno", "mpb",
+    "new-age", "opera", "pagode", "party", "piano",
+    "pop", "pop-film", "power-pop", "progressive-house", "psych-rock",
+    "punk", "punk-rock", "r-n-b", "reggae", "reggaeton",
+    "rock", "rock-n-roll", "rockabilly", "romance", "sad",
+    "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter",
+    "ska", "sleep", "songwriter", "soul", "spanish",
+    "study", "swedish", "synth-pop", "tango", "techno",
+    "trance", "trip-hop", "turkish", "world-music"};
+
   char genero[100];
-
-  // Solicita al usuario el ID de la película
-  printf("Ingrese el género de la película: ");
-  scanf("%s", genero); // Lee el ID del teclado
-
-  MapPair *pair = map_search(music_bygenres, genero);
   
-  if (pair != NULL) {
-      List* pelis = pair->value;
-      Film *peli = list_first(pelis);
+  printf("Ingrese el género de la cancion: ");
+  scanf("%s", genero);  
+  MapPair *pair = map_search(music_bygenres, genero);
+
+  while(pair == NULL){
+    printf("¡¡¡El genero ingresado no se ha encontrado, porfa ingrese uno valido!!!\n");
+
+    printf("mostrando generos musicales disponibles:\n");
+    for(int i = 0; i < 114; i++){
+      printf("%s; ",genres[i]);
+      if(i % 14 == 0 && i != 0) printf("\n");
+    }
+    printf("Ingrese el género de la cancion: ");
+    scanf("%s", genero);  
+    pair = map_search(music_bygenres, genero);
+  }
+  
+  List* canciones = pair->value;
+  Song *cancion = list_first(canciones);
       
-      while (peli != NULL) {
-        printf("ID: %s, Título: %s, Director: %s, Año: %d\n", peli->id, peli->artists,
-           peli->director, peli->year);
-        peli = list_next(pelis);
-      }
+  while (cancion != NULL) {
+    printf("ID: %s, Artista: %s, Album: %s, Track: %s, Tempo: %f, Track Genre: %s\n", cancion->id, cancion->artists, cancion->album_name, 
+    cancion->track_name, cancion->tempo, cancion->track_genre);
+    cancion = list_next(canciones);
   }
 }
-*/
+
 int main() {
   char opcion; // Variable para almacenar una opción ingresada por el usuario
                // (sin uso en este fragmento)
@@ -172,22 +213,27 @@ int main() {
 
   // Recuerda usar un mapa por criterio de búsqueda
 
+  bool cargadas = false;
   do {
     mostrarMenuPrincipal();
     printf("Ingrese su opción: ");
     scanf(" %c", &opcion);
-
     switch (opcion) {
     case '1':
-      cargar_musica(music_byid, music_bygenres);
+      if(cargadas) printf("Ya se cargaron aweonao\n");
+      else {
+        cargadas = true;
+        cargar_musica(music_byid, music_bygenres);
+      }  
       break;
     case '2':
-      //buscar_por_id(music_byid);
+      buscar_por_genero(music_bygenres);
       break;
     case '3':
+      //buscar_por_artista(music_byid);
       break;
     case '4':
-      //buscar_por_genero(music_bygenres);
+      //buscar_por_tempo(music_byid);
       break;
     case '5':
       break;
