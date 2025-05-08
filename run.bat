d130bf19837383cdcb7a195490224eaf2258b12c
Your Name <you@example.com>
2025-05-08 01:02:59 -0400

    rename
    Co-authored-by: Franco Bernal <FranKezu@users.noreply.github.com>

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-gcc tarea2.c tdas\list.c tdas\map.c tdas\extra.c tdas\hashmap.c -o tarea2
-tarea2.exe
+gcc spotifind.c tdas\list.c tdas\map.c tdas\extra.c tdas\hashmap.c -o spotifind
+spotifind.exe
 
 pause
